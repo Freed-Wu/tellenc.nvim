cbbd050c09e84ba117b66179e2c183e3b0edd64d
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-07-14 02:37:26 +0800

    :heavy_minus_sign: Remove luarocks-build-xmake

--- xmake.lua
+++ xmake.lua
@@ -6,7 +6,7 @@ add_rules("mode.debug", "mode.release")
 target("tellenc")
 do
     add_includedirs(".")
-    add_rules("luarocks.module", "lua.native-objects")
+    add_rules("lua.module", "lua.native-objects")
     add_files("*.cpp", "*.nobj.lua")
     add_links("stdc++")
 end
